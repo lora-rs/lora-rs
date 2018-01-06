bb522c6e0400401fbc6ed0eccf84203414efb6c1
Ivaylo Petrov <ivajloip@gmail.com>
2018-01-06 15:04:29 +0100

    Improve copyright statement

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+// Copyright (c) 2017,2018 Ivaylo Petrov
+//
 // Licensed under the MIT license <LICENSE-MIT or
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
