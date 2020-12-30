9c1d3b58db573ae23aa8cd17caaca98d6f6ce5e5
Ivaylo Petrov <ivajloip@gmail.com>
2020-12-30 22:39:22 +0100

    Backport: PR #31 fix long payload enc/dec
    Commit d5f7cb4

--- tests/lorawan.rs
+++ tests/lorawan.rs
@@ -9,11 +9,11 @@
 extern crate lorawan;
 
 use lorawan::creator::*;
+use lorawan::default_crypto::DefaultFactory;
 use lorawan::keys::*;
 use lorawan::maccommandcreator::*;
 use lorawan::maccommands::*;
 use lorawan::parser::*;
-use lorawan::default_crypto::DefaultFactory;
 
 fn phy_join_request_payload() -> Vec<u8> {
     let mut res = Vec::new();
@@ -38,7 +38,8 @@ fn phy_join_accept_payload_with_c_f_list() -> Vec<u8> {
     res.extend_from_slice(&[
         0x20, 0xe4, 0x56, 0x73, 0xb6, 0x3c, 0xb4, 0xb9, 0xce, 0xcb, 0x2a, 0xa8, 0x3f, 0x03, 0x33,
         0xe6, 0x15, 0xd2, 0xac, 0x89, 0xee, 0xa1, 0x65, 0x98, 0x37, 0xc3, 0xaa, 0x6d, 0xf9, 0x68,
-        0x98, 0x89, 0xcf]);
+        0x98, 0x89, 0xcf,
+    ]);
     res
     //867100000, 867300000, 867500000, 867700000, 867900000
 }
@@ -52,11 +53,45 @@ fn phy_dataup_payload() -> Vec<u8> {
     res
 }
 
+fn phy_long_dataup_payload() -> Vec<u8> {
+    let mut res = Vec::new();
+    res.extend_from_slice(&[
+        0x40, 0x04, 0x03, 0x02, 0x01, 0x00, 0x00, 0x00, 0x01, 0x27, 0x5a, 0xe9, 0x94, 0x2a, 0x58,
+        0x32, 0x21, 0x48, 0xba, 0xd6, 0xca, 0x7d, 0x74, 0x6e, 0x77, 0x4a, 0xf8, 0x66, 0x7a, 0x7b,
+        0x72, 0x36, 0x4b, 0xe4, 0xe1, 0x9d, 0x2f, 0x5c, 0x23, 0x98, 0x4f, 0xe2, 0x5e, 0x8e, 0x2d,
+        0xdb, 0xd5, 0x15, 0xb5, 0x4e, 0xbe, 0x80, 0xce, 0xc2, 0x1c, 0xd6, 0x5a, 0x88, 0x13, 0x0f,
+        0xbe, 0x6d, 0x04, 0xaa, 0xb2, 0xbc, 0x39, 0xab, 0xbe, 0xd9, 0xe8, 0x73, 0xef, 0xc7, 0x85,
+        0xe5, 0x65, 0x5d, 0x62, 0x72, 0xf8, 0x79, 0x6b, 0x1e, 0x83, 0x9f, 0x2b, 0x1b, 0xde, 0xab,
+        0xa2, 0x01, 0x6c, 0x7e, 0xf9, 0x16, 0x9d, 0x51, 0xf4, 0xea, 0x26, 0x1b, 0xc6, 0x08, 0x9c,
+        0x83, 0xb3, 0x3c, 0x6f, 0x30, 0xa7, 0x3c, 0xe1, 0x3c, 0x52, 0x55, 0x7c, 0x46, 0xd7, 0x91,
+        0xe7, 0xe0, 0x1b, 0x39, 0xe0, 0xb8, 0x9c, 0x1d, 0x2e, 0x35, 0x08, 0x84, 0x1b, 0x67, 0xe3,
+        0xec, 0x88, 0x6f, 0x96, 0xeb, 0x0e, 0x11, 0x16, 0x40, 0xd3, 0xc1, 0x94, 0xf1, 0x21, 0x49,
+        0xab, 0x58, 0x4b, 0xd9, 0x31, 0xdc, 0x15, 0xfc, 0x11, 0x94, 0x97, 0xdc, 0xcb, 0xf2, 0xb5,
+        0xb9, 0x16, 0xb8, 0x52, 0x42, 0x96, 0x33, 0x41, 0xa5, 0x8b, 0xb5, 0x87, 0x7b, 0xd5, 0xaf,
+        0x9e, 0xe4, 0x2d, 0x8b, 0x6f, 0x48, 0x45, 0x85, 0xa6, 0xf9, 0xcb, 0xaf, 0xf7, 0x2e, 0xe1,
+        0x09, 0x42, 0xe1, 0x23, 0x8c, 0x98, 0xd7, 0xbf, 0xe7, 0xca, 0x0b, 0x2d, 0xb2, 0x24, 0x8d,
+        0xb9, 0x1c, 0xd2, 0x3a, 0x71, 0xc6, 0xdb, 0x9b, 0x76, 0x8c, 0xf7, 0xef, 0x17, 0xf0, 0x51,
+        0xcf, 0x42, 0x3e, 0x73, 0x47, 0x7a, 0xbc, 0x9b, 0x0f, 0xf0, 0x62, 0xde, 0x1e, 0x85, 0x20,
+        0x29, 0x92, 0xdd, 0xca, 0x58, 0x37, 0x44, 0x19, 0x0c, 0x4f, 0xf7, 0xe1, 0xb4, 0x2e, 0xa3,
+        0xcc,
+    ]);
+    res
+}
+
+fn long_data_payload() -> String {
+    // some text from loremipsum.de with a typo at the end
+    String::from(
+        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor \
+            invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et \
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, not",
+    )
+}
+
 fn phy_datadown_payload() -> Vec<u8> {
     let mut res = Vec::new();
     res.extend_from_slice(&[
         0xa0, 0x04, 0x03, 0x02, 0x01, 0x80, 0xff, 0x2a, 0x2a, 0x0a, 0xf1, 0xa3, 0x6a, 0x05, 0xd0,
-        0x12, 0x5f, 0x88, 0x5d, 0x88, 0x1d, 0x49, 0xe1
+        0x12, 0x5f, 0x88, 0x5d, 0x88, 0x1d, 0x49, 0xe1,
     ]);
     res
 }
@@ -73,7 +108,7 @@ fn data_payload_with_fport_zero() -> Vec<u8> {
 fn data_payload_with_f_opts() -> Vec<u8> {
     let mut res = Vec::new();
     res.extend_from_slice(&[
-        0x40, 0x04, 0x03, 0x02, 0x01, 0x03, 0x00, 0x00, 0x02, 0x03, 0x05, 0xd7, 0xfa, 0x0c, 0x6c
+        0x40, 0x04, 0x03, 0x02, 0x01, 0x03, 0x00, 0x00, 0x02, 0x03, 0x05, 0xd7, 0xfa, 0x0c, 0x6c,
     ]);
     res
 }
@@ -115,7 +150,7 @@ fn test_mhdr_major() {
 #[test]
 fn test_parse_phy_payload_with_too_few_bytes_is_err() {
     let bytes = vec![
-        0x80, 0x04, 0x03, 0x02, 0x01, 0x00, 0xff, 0x01, 0x02, 0x03, 0x04
+        0x80, 0x04, 0x03, 0x02, 0x01, 0x00, 0xff, 0x01, 0x02, 0x03, 0x04,
     ];
     let phy = parse(bytes);
     assert!(phy.is_err());
@@ -126,7 +161,9 @@ fn test_parse_join_request_payload() {
     let phy = parse(phy_join_request_payload());
     assert_eq!(
         phy,
-        Ok(PhyPayload::JoinRequest(JoinRequestPayload::new(phy_join_request_payload()).unwrap()))
+        Ok(PhyPayload::JoinRequest(
+            JoinRequestPayload::new(phy_join_request_payload()).unwrap()
+        ))
     );
 }
 
@@ -136,7 +173,8 @@ fn test_parse_join_accept_payload() {
     assert_eq!(
         phy,
         Ok(PhyPayload::JoinAccept(JoinAcceptPayload::Encrypted(
-                    EncryptedJoinAcceptPayload::new(phy_join_accept_payload()).unwrap())))
+            EncryptedJoinAcceptPayload::new(phy_join_accept_payload()).unwrap()
+        )))
     );
 }
 
@@ -146,7 +184,8 @@ fn test_parse_data_payload() {
     assert_eq!(
         phy,
         Ok(PhyPayload::Data(DataPayload::Encrypted(
-                    EncryptedDataPayload::new(phy_dataup_payload()).unwrap())))
+            EncryptedDataPayload::new(phy_dataup_payload()).unwrap()
+        )))
     );
 }
 
@@ -182,7 +221,10 @@ fn test_new_join_accept_c_f_list_empty() {
 fn test_join_accept_app_nonce_extraction() {
     let decrypted_phy = new_decrypted_join_accept();
     let expected = vec![3, 2, 1];
-    assert_eq!(decrypted_phy.app_nonce(), AppNonce::new(&expected[..]).unwrap());
+    assert_eq!(
+        decrypted_phy.app_nonce(),
+        AppNonce::new(&expected[..]).unwrap()
+    );
 }
 
 #[test]
@@ -210,9 +252,13 @@ fn test_new_join_accept_payload_with_c_f_list() {
     let key = AES128([1; 16]);
     let decrypted_phy = DecryptedJoinAcceptPayload::new(data, &key).unwrap();
 
-    let expected_c_f_list = [Frequency::new_from_raw(&[0x18, 0x4F, 0x84]),
-        Frequency::new_from_raw(&[0xE8, 0x56, 0x84]), Frequency::new_from_raw(&[0xB8, 0x5E, 0x84]),
-        Frequency::new_from_raw(&[0x88, 0x66, 0x84]), Frequency::new_from_raw(&[0x58, 0x6E, 0x84])];
+    let expected_c_f_list = [
+        Frequency::new_from_raw(&[0x18, 0x4F, 0x84]),
+        Frequency::new_from_raw(&[0xE8, 0x56, 0x84]),
+        Frequency::new_from_raw(&[0xB8, 0x5E, 0x84]),
+        Frequency::new_from_raw(&[0x88, 0x66, 0x84]),
+        Frequency::new_from_raw(&[0x58, 0x6E, 0x84]),
+    ];
     assert_eq!(decrypted_phy.c_f_list(), Some(expected_c_f_list));
 }
 
@@ -244,9 +290,12 @@ fn test_validate_data_mic_when_not_ok() {
 
 #[test]
 fn test_new_data_payload_is_none_if_bytes_too_short() {
-    let bytes = &[0x80, 0x04, 0x03, 0x02, 0x01, 0x00, 0xff, 0x01, 0x02, 0x03, 0x04];
-    let bytes_with_fopts =
-        &[0x00, 0x04, 0x03, 0x02, 0x01, 0x01, 0xff, 0x04, 0x01, 0x02, 0x03, 0x04];
+    let bytes = &[
+        0x80, 0x04, 0x03, 0x02, 0x01, 0x00, 0xff, 0x01, 0x02, 0x03, 0x04,
+    ];
+    let bytes_with_fopts = &[
+        0x00, 0x04, 0x03, 0x02, 0x01, 0x01, 0xff, 0x04, 0x01, 0x02, 0x03, 0x04,
+    ];
 
     assert!(EncryptedDataPayload::new(bytes).is_err());
     assert!(EncryptedDataPayload::new(bytes_with_fopts).is_err());
@@ -254,7 +303,9 @@ fn test_new_data_payload_is_none_if_bytes_too_short() {
 
 #[test]
 fn test_f_port_could_be_absent_in_data_payload() {
-    let bytes = &[0x80, 0x04, 0x03, 0x02, 0x01, 0x00, 0xff, 0x04, 0x01, 0x02, 0x03, 0x04];
+    let bytes = &[
+        0x80, 0x04, 0x03, 0x02, 0x01, 0x00, 0xff, 0x04, 0x01, 0x02, 0x03, 0x04,
+    ];
     let data_payload = EncryptedDataPayload::new(bytes).unwrap();
     assert!(data_payload.f_port().is_none());
 }
@@ -263,9 +314,12 @@ fn test_f_port_could_be_absent_in_data_payload() {
 fn test_complete_data_payload_fhdr() {
     let app_skey = AES128([1; 16]);
     let nwk_skey = AES128([2; 16]);
-    let phys: std::vec::Vec<Box<dyn DataHeader>> =
-        vec![Box::new(EncryptedDataPayload::new(phy_dataup_payload()).unwrap()),
-            Box::new(DecryptedDataPayload::new(phy_dataup_payload(), &nwk_skey, Some(&app_skey), 1).unwrap())];
+    let phys: std::vec::Vec<Box<dyn DataHeader>> = vec![
+        Box::new(EncryptedDataPayload::new(phy_dataup_payload()).unwrap()),
+        Box::new(
+            DecryptedDataPayload::new(phy_dataup_payload(), &nwk_skey, Some(&app_skey), 1).unwrap(),
+        ),
+    ];
     for phy in phys {
         assert_eq!(phy.f_port(), Some(1));
 
@@ -299,6 +353,18 @@ fn test_complete_dataup_payload_frm_payload() {
 }
 
 #[test]
+fn test_complete_long_dataup_payload_frm_payload() {
+    let phy = EncryptedDataPayload::new(phy_long_dataup_payload()).unwrap();
+    let nwk_skey = AES128([2; 16]);
+    let app_skey = AES128([1; 16]);
+    let decrypted = phy.decrypt_if_mic_ok(&nwk_skey, &app_skey, 0).unwrap();
+    let mut payload = Vec::new();
+    payload.extend_from_slice(&long_data_payload().into_bytes()[..]);
+
+    assert_eq!(decrypted.frm_payload(), Ok(FRMPayload::Data(&payload)));
+}
+
+#[test]
 fn test_complete_datadown_payload_frm_payload() {
     let phy = EncryptedDataPayload::new(phy_datadown_payload()).unwrap();
     let key = AES128([1; 16]);
@@ -311,8 +377,10 @@ fn test_complete_datadown_payload_frm_payload() {
 
 #[test]
 fn test_mac_command_in_downlink() {
-    let data = [0x60, 0x5f, 0x3b, 0xd7, 0x4e, 0x0a, 0x00, 0x00, 0x03, 0x00, 0x00,
-    0x00, 0x70, 0x03, 0x00, 0xff, 0x00, 0x30, 0xcd, 0xdb, 0x22, 0xee];
+    let data = [
+        0x60, 0x5f, 0x3b, 0xd7, 0x4e, 0x0a, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x70, 0x03, 0x00,
+        0xff, 0x00, 0x30, 0xcd, 0xdb, 0x22, 0xee,
+    ];
     let packet = EncryptedDataPayload::new(data).unwrap();
 
     assert_eq!(packet.mhdr().mtype(), MType::UnconfirmedDataDown);
@@ -382,6 +450,31 @@ fn test_data_payload_uplink_creator() {
     assert_eq!(
         phy.build(b"hello", &[], &nwk_skey, &app_skey).unwrap(),
         &phy_dataup_payload()[..]
+    );
+}
+
+#[test]
+fn test_long_data_payload_uplink_creator() {
+    let mut phy = DataPayloadCreator::new();
+    let nwk_skey = AES128([2; 16]);
+    let app_skey = AES128([1; 16]);
+    let fctrl = FCtrl::new(0x00, true);
+    phy.set_confirmed(false)
+        .set_uplink(true)
+        .set_f_port(1)
+        .set_dev_addr(&[4, 3, 2, 1])
+        .set_fctrl(&fctrl) // all flags set to false
+        .set_fcnt(0);
+
+    assert_eq!(
+        phy.build(
+            &long_data_payload().into_bytes()[..],
+            &[],
+            &nwk_skey,
+            &app_skey
+        )
+        .unwrap(),
+        &phy_long_dataup_payload()[..]
     );
 }
 
@@ -563,28 +656,32 @@ fn test_join_request_creator_with_options() {
 }
 
 #[test]
-fn test_derive_newskey(){
+fn test_derive_newskey() {
     let key = AES128(app_key());
     let join_request = JoinRequestPayload::new(phy_join_request_payload()).unwrap();
     let join_accept = DecryptedJoinAcceptPayload::new(phy_join_accept_payload(), &key).unwrap();
 
     let newskey = join_accept.derive_newskey(&join_request.dev_nonce(), &key);
     //AppNonce([49, 3e, eb]), NwkAddr([51, fb, a2]), DevNonce([2d, 10])
-    let expect = [0x7b, 0xb2, 0x5f, 0x89, 0xe0, 0xd1, 0x37, 0x1e, 0x1f, 0xbf, 0x4d, 0x99, 0x7e,
-        0x14, 0x68, 0xa3];
+    let expect = [
+        0x7b, 0xb2, 0x5f, 0x89, 0xe0, 0xd1, 0x37, 0x1e, 0x1f, 0xbf, 0x4d, 0x99, 0x7e, 0x14, 0x68,
+        0xa3,
+    ];
     assert_eq!(newskey.0, expect);
 }
 
 #[test]
-fn test_derive_appskey(){
+fn test_derive_appskey() {
     let key = AES128(app_key());
     let join_request = JoinRequestPayload::new(phy_join_request_payload()).unwrap();
     let join_accept = DecryptedJoinAcceptPayload::new(phy_join_accept_payload(), &key).unwrap();
 
     let appskey = join_accept.derive_appskey(&join_request.dev_nonce(), &key);
     //AppNonce([49, 3e, eb]), NwkAddr([51, fb, a2]), DevNonce([2d, 10])
-    let expect = [0x14, 0x88, 0x20, 0xdf, 0xb1, 0xe0, 0xc9, 0xd6, 0x28, 0x9c, 0xde, 0x16, 0xc1,
-        0xaf, 0x24, 0x9f];
+    let expect = [
+        0x14, 0x88, 0x20, 0xdf, 0xb1, 0xe0, 0xc9, 0xd6, 0x28, 0x9c, 0xde, 0x16, 0xc1, 0xaf, 0x24,
+        0x9f,
+    ];
 
     assert_eq!(appskey.0, expect);
 }
