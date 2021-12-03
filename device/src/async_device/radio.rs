7e331af2c5a5c6c3e6d3391cb55256105b308b40
Ulf Lilleengen <lulf@redhat.com>
2021-12-03 10:11:11 +0100

    Improve async radio trait to allow non-static impls

--- device/src/async_device/radio.rs
+++ device/src/async_device/radio.rs
@@ -28,13 +28,17 @@ pub trait PhyRxTx: Sized {
     #[cfg(not(feature = "defmt"))]
     type PhyError;
 
-    type TxFuture<'m>: Future<Output = Result<u32, Self::PhyError>> + 'm;
+    type TxFuture<'m>: Future<Output = Result<u32, Self::PhyError>> + 'm
+    where
+        Self: 'm;
 
     /// Transmit data buffer with the given tranciever configuration. The returned future
     /// should only complete once data have been transmitted.
     fn tx<'m>(&'m mut self, config: TxConfig, buf: &'m [u8]) -> Self::TxFuture<'m>;
 
-    type RxFuture<'m>: Future<Output = Result<(usize, RxQuality), Self::PhyError>> + 'm;
+    type RxFuture<'m>: Future<Output = Result<(usize, RxQuality), Self::PhyError>> + 'm
+    where
+        Self: 'm;
     /// Receive data into the provided buffer with the given tranciever configuration. The returned future
     /// should only complete when RX data have been received.
     fn rx<'m>(&'m mut self, config: RfConfig, rx_buf: &'m mut [u8]) -> Self::RxFuture<'m>;
