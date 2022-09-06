73ae329da102626908d6a21ca5746416c8b94824
lthiery <thiery.louis@gmail.com>
2022-09-06 08:40:33 -0700

    adopt proper datarate when joining on fat channels

--- device/src/region/au915/mod.rs
+++ device/src/region/au915/mod.rs
@@ -52,25 +52,30 @@ impl RegionHandler for AU915 {
         datarate: DR,
         frame: &Frame,
     ) -> (Datarate, u32) {
+        let subband_channel = random & 0b111;
+        let subband = if datarate == DR::_4 {
+            8
+        } else if let Some(subband) = &self.subband {
+            subband - 1
+        } else {
+            (random >> 3) & 0b111
+        };
+        self.last_tx = (subband, subband_channel);
+
         (
             {
                 let datarate = match frame {
-                    // datarate for JoinRequest is always 0
-                    Frame::Join => DR::_0,
+                    Frame::Join => if subband==7 {
+                        DR::_2
+                    } else {
+                        DR::_0
+                    },
                     Frame::Data => datarate,
                 };
                 DATARATES[datarate as usize].clone().unwrap()
             },
             {
-                let subband_channel = random & 0b111;
-                let subband = if datarate == DR::_4 {
-                    8
-                } else if let Some(subband) = &self.subband {
-                    subband - 1
-                } else {
-                    (random >> 3) & 0b111
-                };
-                self.last_tx = (subband, subband_channel);
+
                 UPLINK_CHANNEL_MAP[subband as usize][subband_channel as usize]
             },
         )
