40e34d7cb0b554c12da3332276492e491f518593
ceekdee <taigatensor@gmail.com>
2023-03-20 11:23:51 -0500

    Add Rp Pico support.

--- src/mod_params.rs
+++ src/mod_params.rs
@@ -54,6 +54,7 @@ pub struct PacketStatus {
 pub enum BoardType {
     GenericSx1261, // placeholder for Sx1261-specific features
     HeltecWifiLoraV31262,
+    RpPicoWaveshareSx1262,
     Rak4631Sx1262,
     Stm32l0Sx1276,
     Stm32wlSx1262,
@@ -76,6 +77,7 @@ impl From<BoardType> for ChipType {
         match board_type {
             BoardType::GenericSx1261 => ChipType::Sx1261,
             BoardType::HeltecWifiLoraV31262 => ChipType::Sx1262,
+            BoardType::RpPicoWaveshareSx1262 => ChipType::Sx1262,
             BoardType::Rak4631Sx1262 => ChipType::Sx1262,
             BoardType::Stm32l0Sx1276 => ChipType::Sx1276,
             BoardType::Stm32wlSx1262 => ChipType::Sx1262,
