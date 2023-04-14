24c8b9374a3ee2b6d8b9c89e06004ce2c580afc0
Ulf Lilleengen <lulf@redhat.com>
2023-04-14 22:48:15 +0200

    Convert async traits to use async-functions in traits

--- device/src/async_device/radio.rs
+++ device/src/async_device/radio.rs
@@ -1,5 +1,4 @@
 pub use crate::radio::{types::*, RfConfig, RxQuality, TxConfig};
-use core::future::Future;
 
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct Error<E>(pub E);
@@ -15,18 +14,11 @@ impl<R> From<Error<R>> for super::Error<R> {
 pub trait Timer {
     fn reset(&mut self);
 
-    type AtFuture<'m>: Future<Output = ()> + 'm
-    where
-        Self: 'm;
-
     /// Wait until millis milliseconds after reset has passed
-    fn at(&mut self, millis: u64) -> Self::AtFuture<'_>;
+    async fn at(&mut self, millis: u64);
 
-    type DelayFuture<'m>: Future<Output = ()> + 'm
-    where
-        Self: 'm;
     /// Delay for millis milliseconds
-    fn delay_ms(&mut self, millis: u64) -> Self::DelayFuture<'_>;
+    async fn delay_ms(&mut self, millis: u64);
 }
 
 /// An asynchronous radio implementation that can transmit and receive data.
@@ -37,18 +29,15 @@ pub trait PhyRxTx: Sized {
     #[cfg(not(feature = "defmt"))]
     type PhyError;
 
-    type TxFuture<'m>: Future<Output = Result<u32, Self::PhyError>> + 'm
-    where
-        Self: 'm;
-
     /// Transmit data buffer with the given tranciever configuration. The returned future
     /// should only complete once data have been transmitted.
-    fn tx<'m>(&'m mut self, config: TxConfig, buf: &'m [u8]) -> Self::TxFuture<'m>;
+    async fn tx(&mut self, config: TxConfig, buf: &[u8]) -> Result<u32, Self::PhyError>;
 
-    type RxFuture<'m>: Future<Output = Result<(usize, RxQuality), Self::PhyError>> + 'm
-    where
-        Self: 'm;
     /// Receive data into the provided buffer with the given tranciever configuration. The returned future
     /// should only complete when RX data have been received.
-    fn rx<'m>(&'m mut self, config: RfConfig, rx_buf: &'m mut [u8]) -> Self::RxFuture<'m>;
+    async fn rx(
+        &mut self,
+        config: RfConfig,
+        rx_buf: &mut [u8],
+    ) -> Result<(usize, RxQuality), Self::PhyError>;
 }
--- device/src/lib.rs
+++ device/src/lib.rs
@@ -1,4 +1,9 @@
 #![no_std]
+#![cfg_attr(
+    feature = "async",
+    feature(async_fn_in_trait)
+)]
+#![allow(incomplete_features)]
 
 use heapless::Vec;
 
