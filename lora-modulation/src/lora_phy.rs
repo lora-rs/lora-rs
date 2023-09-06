93990eb4918f9837872b36a954746a6c5c7bdbf5
lthiery <thiery.louis@gmail.com>
2023-09-06 13:17:32 -0700

    complete mapping of bandwidths

--- lora-modulation/src/lora_phy.rs
+++ lora-modulation/src/lora_phy.rs
@@ -6,14 +6,14 @@ use ::lora_phy::mod_params;
 impl From<SpreadingFactor> for mod_params::SpreadingFactor {
     fn from(sf: SpreadingFactor) -> Self {
         match sf {
-            SpreadingFactor::_5 => lora_phy::mod_params::SpreadingFactor::_5,
-            SpreadingFactor::_6 => lora_phy::mod_params::SpreadingFactor::_6,
-            SpreadingFactor::_7 => lora_phy::mod_params::SpreadingFactor::_7,
-            SpreadingFactor::_8 => lora_phy::mod_params::SpreadingFactor::_8,
-            SpreadingFactor::_9 => lora_phy::mod_params::SpreadingFactor::_9,
-            SpreadingFactor::_10 => lora_phy::mod_params::SpreadingFactor::_10,
-            SpreadingFactor::_11 => lora_phy::mod_params::SpreadingFactor::_11,
-            SpreadingFactor::_12 => lora_phy::mod_params::SpreadingFactor::_12,
+            SpreadingFactor::_5 => mod_params::SpreadingFactor::_5,
+            SpreadingFactor::_6 => mod_params::SpreadingFactor::_6,
+            SpreadingFactor::_7 => mod_params::SpreadingFactor::_7,
+            SpreadingFactor::_8 => mod_params::SpreadingFactor::_8,
+            SpreadingFactor::_9 => mod_params::SpreadingFactor::_9,
+            SpreadingFactor::_10 => mod_params::SpreadingFactor::_10,
+            SpreadingFactor::_11 => mod_params::SpreadingFactor::_11,
+            SpreadingFactor::_12 => mod_params::SpreadingFactor::_12,
         }
     }
 }
@@ -22,9 +22,16 @@ impl From<SpreadingFactor> for mod_params::SpreadingFactor {
 impl From<Bandwidth> for mod_params::Bandwidth {
     fn from(bw: Bandwidth) -> Self {
         match bw {
-            Bandwidth::_125KHz => lora_phy::mod_params::Bandwidth::_125KHz,
-            Bandwidth::_250KHz => lora_phy::mod_params::Bandwidth::_250KHz,
-            Bandwidth::_500KHz => lora_phy::mod_params::Bandwidth::_500KHz,
+            Bandwidth::_7KHz => mod_params::Bandwidth::_7KHz,
+            Bandwidth::_10KHz => mod_params::Bandwidth::_10KHz,
+            Bandwidth::_15KHz => mod_params::Bandwidth::_15KHz,
+            Bandwidth::_20KHz => mod_params::Bandwidth::_20KHz,
+            Bandwidth::_31KHz => mod_params::Bandwidth::_31KHz,
+            Bandwidth::_41KHz => mod_params::Bandwidth::_41KHz,
+            Bandwidth::_62KHz => mod_params::Bandwidth::_62KHz,
+            Bandwidth::_125KHz => mod_params::Bandwidth::_125KHz,
+            Bandwidth::_250KHz => mod_params::Bandwidth::_250KHz,
+            Bandwidth::_500KHz => mod_params::Bandwidth::_500KHz,
         }
     }
 }
@@ -33,10 +40,10 @@ impl From<Bandwidth> for mod_params::Bandwidth {
 impl From<CodingRate> for mod_params::CodingRate {
     fn from(cr: CodingRate) -> Self {
         match cr {
-            CodingRate::_4_5 => lora_phy::mod_params::CodingRate::_4_5,
-            CodingRate::_4_6 => lora_phy::mod_params::CodingRate::_4_6,
-            CodingRate::_4_7 => lora_phy::mod_params::CodingRate::_4_7,
-            CodingRate::_4_8 => lora_phy::mod_params::CodingRate::_4_8,
+            CodingRate::_4_5 => mod_params::CodingRate::_4_5,
+            CodingRate::_4_6 => mod_params::CodingRate::_4_6,
+            CodingRate::_4_7 => mod_params::CodingRate::_4_7,
+            CodingRate::_4_8 => mod_params::CodingRate::_4_8,
         }
     }
 }
