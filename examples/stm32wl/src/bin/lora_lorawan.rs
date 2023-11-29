78d73f4c7341a1ac8a22e7b33df635242e6c2c16
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2023-11-29 21:04:32 +0100

    examples: Add stm32wl

---
+++ examples/stm32wl/src/bin/lora_lorawan.rs
@@ -0,0 +1,91 @@
+//! This example runs on a STM32WL board, which has a builtin Semtech Sx1262 radio.
+//! It demonstrates LoRaWAN join functionality.
+#![no_std]
+#![no_main]
+
+#[path = "../iv.rs"]
+mod iv;
+
+use defmt::info;
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Pin, Speed};
+use embassy_stm32::rng::{self, Rng};
+use embassy_stm32::spi::Spi;
+use embassy_stm32::time::Hertz;
+use embassy_stm32::{bind_interrupts, peripherals};
+use embassy_time::Delay;
+use lora_phy::sx1261_2::{self, Sx126xVariant, TcxoCtrlVoltage, SX1261_2};
+use lora_phy::LoRa;
+use lorawan::default_crypto::DefaultFactory as Crypto;
+use lorawan_device::async_device::lora_radio::LoRaRadio;
+use lorawan_device::async_device::{region, Device, EmbassyTimer, JoinMode};
+use lorawan_device::{AppEui, AppKey, DevEui};
+use {defmt_rtt as _, panic_probe as _};
+
+use self::iv::{InterruptHandler, Stm32wlInterfaceVariant, SubghzSpiDevice};
+
+// warning: set these appropriately for the region
+const LORAWAN_REGION: region::Region = region::Region::EU868;
+const MAX_TX_POWER: u8 = 14;
+
+bind_interrupts!(struct Irqs{
+    SUBGHZ_RADIO => InterruptHandler;
+    RNG => rng::InterruptHandler<peripherals::RNG>;
+});
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let mut config = embassy_stm32::Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        config.rcc.hse = Some(Hse {
+            freq: Hertz(32_000_000),
+            mode: HseMode::Bypass,
+            prescaler: HsePrescaler::DIV1,
+        });
+        config.rcc.mux = ClockSrc::PLL1_R;
+        config.rcc.pll = Some(Pll {
+            source: PllSource::HSE,
+            prediv: PllPreDiv::DIV2,
+            mul: PllMul::MUL6,
+            divp: None,
+            divq: Some(PllQDiv::DIV2), // PLL1_Q clock (32 / 2 * 6 / 2), used for RNG
+            divr: Some(PllRDiv::DIV2), // sysclk 48Mhz clock (32 / 2 * 6 / 2)
+        });
+    }
+    let p = embassy_stm32::init(config);
+
+    // Set CTRL1 and CTRL3 for high-power transmission, while CTRL2 acts as an RF switch between tx and rx
+    let _ctrl1 = Output::new(p.PC4.degrade(), Level::Low, Speed::High);
+    let ctrl2 = Output::new(p.PC5.degrade(), Level::High, Speed::High);
+    let _ctrl3 = Output::new(p.PC3.degrade(), Level::High, Speed::High);
+
+    let spi = Spi::new_subghz(p.SUBGHZSPI, p.DMA1_CH1, p.DMA1_CH2);
+    let spi = SubghzSpiDevice(spi);
+
+    let config = sx1261_2::Config {
+        chip: Sx126xVariant::Stm32wl,
+        tcxo_ctrl: Some(TcxoCtrlVoltage::Ctrl1V7),
+        use_dcdc: true,
+    };
+    let iv = Stm32wlInterfaceVariant::new(Irqs, None, Some(ctrl2)).unwrap();
+    let lora = LoRa::new(SX1261_2::new(spi, iv, config), true, Delay).await.unwrap();
+
+    let radio = LoRaRadio::<_, _, MAX_TX_POWER>::new(lora);
+    let region: region::Configuration = region::Configuration::new(LORAWAN_REGION);
+    let mut device: Device<_, Crypto, _, _> = Device::new(region, radio, EmbassyTimer::new(), Rng::new(p.RNG, Irqs));
+
+    defmt::info!("Joining LoRaWAN network");
+
+    // TODO: Adjust the EUI and Keys according to your network credentials
+    let resp = device
+        .join(&JoinMode::OTAA {
+            deveui: DevEui::from([0, 0, 0, 0, 0, 0, 0, 0]),
+            appeui: AppEui::from([0, 0, 0, 0, 0, 0, 0, 0]),
+            appkey: AppKey::from([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+        })
+        .await
+        .unwrap();
+
+    info!("LoRaWAN network joined: {:?}", resp);
+}
