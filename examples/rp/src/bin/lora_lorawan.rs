1f6d98a89abc40199108d851edf1cd0d6ea39b4f
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2023-11-29 20:28:18 +0100

    examples: add rp2040 + sx1262 examples.

---
+++ examples/rp/src/bin/lora_lorawan.rs
@@ -0,0 +1,73 @@
+//! This example runs on the Raspberry Pi Pico with a Waveshare board containing a Semtech Sx1262 radio.
+//! It demonstrates LoRaWAN join functionality.
+
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_rp::gpio::{Input, Level, Output, Pin, Pull};
+use embassy_rp::spi::{Config, Spi};
+use embassy_time::Delay;
+use embedded_hal_bus::spi::ExclusiveDevice;
+use lora_phy::iv::GenericSx126xInterfaceVariant;
+use lora_phy::sx1261_2;
+use lora_phy::sx1261_2::{Sx126xVariant, TcxoCtrlVoltage, SX1261_2};
+use lora_phy::LoRa;
+use lorawan::default_crypto::DefaultFactory as Crypto;
+use lorawan_device::async_device::lora_radio::LoRaRadio;
+use lorawan_device::async_device::{region, Device, EmbassyTimer, JoinMode};
+use lorawan_device::{AppEui, AppKey, DevEui};
+use {defmt_rtt as _, panic_probe as _};
+
+// warning: set these appropriately for the region
+const LORAWAN_REGION: region::Region = region::Region::EU868;
+const MAX_TX_POWER: u8 = 14;
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+
+    let nss = Output::new(p.PIN_3.degrade(), Level::High);
+    let reset = Output::new(p.PIN_15.degrade(), Level::High);
+    let dio1 = Input::new(p.PIN_20.degrade(), Pull::None);
+    let busy = Input::new(p.PIN_2.degrade(), Pull::None);
+
+    let spi = Spi::new(
+        p.SPI1,
+        p.PIN_10,
+        p.PIN_11,
+        p.PIN_12,
+        p.DMA_CH0,
+        p.DMA_CH1,
+        Config::default(),
+    );
+    let spi = ExclusiveDevice::new(spi, nss, Delay);
+
+    let config = sx1261_2::Config {
+        chip: Sx126xVariant::Sx1262,
+        tcxo_ctrl: Some(TcxoCtrlVoltage::Ctrl1V7),
+        use_dcdc: true,
+    };
+    let iv = GenericSx126xInterfaceVariant::new(reset, dio1, busy, None, None).unwrap();
+    let lora = LoRa::new(SX1261_2::new(spi, iv, config), true, Delay).await.unwrap();
+
+    let radio = LoRaRadio::<_, _, MAX_TX_POWER>::new(lora);
+    let region: region::Configuration = region::Configuration::new(LORAWAN_REGION);
+    let mut device: Device<_, Crypto, _, _> =
+        Device::new(region, radio, EmbassyTimer::new(), embassy_rp::clocks::RoscRng);
+
+    defmt::info!("Joining LoRaWAN network");
+
+    // TODO: Adjust the EUI and Keys according to your network credentials
+    let resp = device
+        .join(&JoinMode::OTAA {
+            deveui: DevEui::from([0, 0, 0, 0, 0, 0, 0, 0]),
+            appeui: AppEui::from([0, 0, 0, 0, 0, 0, 0, 0]),
+            appkey: AppKey::from([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+        })
+        .await
+        .unwrap();
+
+    info!("LoRaWAN network joined: {:?}", resp);
+}
---
+++ examples/rp/src/bin/lora_p2p_send.rs
@@ -0,0 +1,97 @@
+//! This example runs on the Raspberry Pi Pico with a Waveshare board containing a Semtech Sx1262 radio.
+//! It demonstrates LORA P2P send functionality.
+
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_rp::gpio::{Input, Level, Output, Pin, Pull};
+use embassy_rp::spi::{Config, Spi};
+use embassy_time::Delay;
+use embedded_hal_bus::spi::ExclusiveDevice;
+use lora_phy::iv::GenericSx126xInterfaceVariant;
+use lora_phy::sx1261_2::{Sx126xVariant, TcxoCtrlVoltage, SX1261_2};
+use lora_phy::LoRa;
+use lora_phy::{mod_params::*, sx1261_2};
+use {defmt_rtt as _, panic_probe as _};
+
+const LORA_FREQUENCY_IN_HZ: u32 = 903_900_000; // warning: set this appropriately for the region
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+
+    let nss = Output::new(p.PIN_3.degrade(), Level::High);
+    let reset = Output::new(p.PIN_15.degrade(), Level::High);
+    let dio1 = Input::new(p.PIN_20.degrade(), Pull::None);
+    let busy = Input::new(p.PIN_2.degrade(), Pull::None);
+
+    let spi = Spi::new(
+        p.SPI1,
+        p.PIN_10,
+        p.PIN_11,
+        p.PIN_12,
+        p.DMA_CH0,
+        p.DMA_CH1,
+        Config::default(),
+    );
+    let spi = ExclusiveDevice::new(spi, nss, Delay);
+
+    let config = sx1261_2::Config {
+        chip: Sx126xVariant::Sx1262,
+        tcxo_ctrl: Some(TcxoCtrlVoltage::Ctrl1V7),
+        use_dcdc: true,
+    };
+    let iv = GenericSx126xInterfaceVariant::new(reset, dio1, busy, None, None).unwrap();
+    let mut lora = LoRa::new(SX1261_2::new(spi, iv, config), true, Delay).await.unwrap();
+
+    let mdltn_params = {
+        match lora.create_modulation_params(
+            SpreadingFactor::_10,
+            Bandwidth::_250KHz,
+            CodingRate::_4_8,
+            LORA_FREQUENCY_IN_HZ,
+        ) {
+            Ok(mp) => mp,
+            Err(err) => {
+                info!("Radio error = {}", err);
+                return;
+            }
+        }
+    };
+
+    let mut tx_pkt_params = {
+        match lora.create_tx_packet_params(4, false, true, false, &mdltn_params) {
+            Ok(pp) => pp,
+            Err(err) => {
+                info!("Radio error = {}", err);
+                return;
+            }
+        }
+    };
+
+    match lora.prepare_for_tx(&mdltn_params, 20, false).await {
+        Ok(()) => {}
+        Err(err) => {
+            info!("Radio error = {}", err);
+            return;
+        }
+    };
+
+    let buffer = [0x01u8, 0x02u8, 0x03u8];
+    match lora.tx(&mdltn_params, &mut tx_pkt_params, &buffer, 0xffffff).await {
+        Ok(()) => {
+            info!("TX DONE");
+        }
+        Err(err) => {
+            info!("Radio error = {}", err);
+            return;
+        }
+    };
+
+    match lora.sleep(false).await {
+        Ok(()) => info!("Sleep successful"),
+        Err(err) => info!("Sleep unsuccessful = {}", err),
+    }
+}
