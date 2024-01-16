ad761bea242d7dd16867e8fbe990c67cb0a8b33c
Priit Laes <plaes@plaes.org>
2024-01-16 13:41:38 +0200

    phy: Move RxMode under mod_params

--- lora-phy/src/mod_params.rs
+++ lora-phy/src/mod_params.rs
@@ -62,6 +62,19 @@ pub enum RadioMode {
     ChannelActivityDetection, // channel activity detection mode
 }
 
+/// Listening mode for LoRaWAN packet detection/reception
+pub enum RxMode {
+    /// Single shot Rx Mode to listen until packet preamble is detected or RxTimeout occurs.
+    /// The device will stay in RX Mode until a packet is received.
+    /// Preamble length as symbols is configured via following registers:
+    /// sx126x: uses `SetLoRaSymbNumTimeout(0 < n < 255)` + `SetStopRxTimerOnPreamble(1)`
+    /// sx127x: uses `RegSymbTimeout (4 < n < 1023)`
+    // TODO: Single mode with time-based timeout is available on sx126x, but not sx127x
+    Single(u16),
+    /// Continuous Rx mode to listen for incoming packets continuously
+    Continuous,
+}
+
 /// Modulation parameters for a send and/or receive communication channel
 pub struct ModulationParams {
     pub(crate) spreading_factor: SpreadingFactor,
