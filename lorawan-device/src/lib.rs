e82529734a2d9468d781139df1ce0baf50f64c73
Priit Laes <plaes@plaes.org>
2024-07-13 23:17:55 +0300

    lorawan-device: Drop `allow(async_fn_in_trait)` as it was stabilized in 1.75
    Also define MSRV in cargo manifest.

--- lorawan-device/src/lib.rs
+++ lorawan-device/src/lib.rs
@@ -1,6 +1,5 @@
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
-#![allow(async_fn_in_trait)]
 
 //! ## Feature flags
 #![doc = document_features::document_features!(feature_label = r#"<span class="stab portability"><code>{feature}</code></span>"#)]
