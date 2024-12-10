9c90bfd26aa5a5b2f83d51ab1d74796a8bbce3b8
Priit Laes <plaes@plaes.org>
2024-12-10 21:03:25 +0200

    macros: Add support for non-zero length payloads

--- lorawan-macros/src/lib.rs
+++ lorawan-macros/src/lib.rs
@@ -4,11 +4,11 @@ use syn::{parse_macro_input, Data, DeriveInput, Fields, Meta, PathArguments};
 
 struct Payload {
     name: Ident,
-    lifetime: Option<Ident>,
+    lifetime: Option<syn::Lifetime>,
 }
 
 impl Payload {
-    fn new(name: Ident, lifetime: Option<Ident>) -> Self {
+    fn new(name: Ident, lifetime: Option<syn::Lifetime>) -> Self {
         Self { name, lifetime }
     }
 }
@@ -16,8 +16,7 @@ impl Payload {
 // TODO: Figure out how to parse value to literal (and handle value sanity checks)
 struct Attributes {
     doc: Vec<syn::Attribute>,
-    cid: Option<syn::Expr>,
-    len: Option<syn::Expr>,
+    attrs: Option<(syn::Expr, syn::Expr)>,
 }
 
 #[proc_macro_derive(CommandHandler, attributes(cmd))]
@@ -26,7 +25,7 @@ pub fn derive_command_handler(input: proc_macro::TokenStream) -> proc_macro::Tok
 
     let name = &input.ident;
 
-    // Parse enum members into list of (Command, Payload) tuples
+    // Parse enum members into list of (Command, Payload, Attributes) tuples
     let members = parse_enum_members(&input);
 
     let mut impl_len = Vec::new();
@@ -39,7 +38,7 @@ pub fn derive_command_handler(input: proc_macro::TokenStream) -> proc_macro::Tok
     for (n, payload, attributes) in members {
         let n = n.clone();
         let t = &payload.name;
-        let _lt = &payload.lifetime;
+        let lt = &payload.lifetime;
         let doc = &attributes.doc;
 
         // ...len()
@@ -65,41 +64,80 @@ pub fn derive_command_handler(input: proc_macro::TokenStream) -> proc_macro::Tok
             } else
         });
 
-        // Generate PayLoad structs (for payloads with zero length)
-        // TODO: Support for payloads where len != 0
-        if attributes.cid.is_some() && attributes.len.is_some() {
-            let cid = attributes.cid.unwrap();
-            let len = attributes.len.unwrap();
-
-            payload_struct_impls.push(quote! {
-                #[derive(Debug, PartialEq, Eq)]
-                #[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
-                #( #doc )*
-                pub struct #t();
-
-                impl #t {
-                    /// Create new
-                    pub fn new(_: &[u8]) -> #t {
-                        #t()
-                    }
-                    /// Create from raw_bytes (for compatibility with non-zero length payloads)
-                    pub fn new_from_raw(_: &[u8]) -> #t {
-                        #t()
-                    }
-                    /// Payload CID
-                    pub const fn cid() -> u8 {
-                        #cid
-                    }
-                    /// Length of empty payload.
-                    pub const fn len() -> usize {
-                        #len
-                    }
-                    /// Reference to the empty payload.
-                    pub fn bytes(&self) -> &[u8] {
-                        &[]
-                    }
+        // Generate definition and common implementation for payloads
+        if let Some((cid, len)) = attributes.attrs {
+            // Generate common code used by zero and non-zero length payloads
+            let common = quote! {
+                /// Payload CID.
+                pub const fn cid() -> u8 {
+                    #cid
                 }
-            });
+                /// Length of payload without the CID.
+                pub const fn len() -> usize {
+                    #len
+                }
+            };
+
+            // Payloads with len > 0 (which have lifetime)
+            if let Some(lt) = lt {
+                payload_struct_impls.push(quote! {
+                    #[derive(Debug, PartialEq, Eq)]
+                    #[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+                    #( #doc )*
+                    pub struct #t <#lt> (pub(crate) &#lt [u8]);
+
+                    impl<#lt> #t<#lt> {
+                        /// Creates a new instance of the MAC command if there is enought data.
+                        pub fn new(data: &#lt [u8]) -> Result<#t<#lt>, Error> {
+                            if data.len() != #len {
+                                Err(Error::BufferTooShort)
+                            } else {
+                                Ok(#t(&data))
+                            }
+                        }
+                        /// Constructs a new instance of the MAC command from the provided data,
+                        /// without verifying the data length.
+                        ///
+                        /// Improper use of this method could lead to panic during runtime!
+                        pub fn new_from_raw(data: &#lt [u8]) ->#t<#lt> {
+                            #t(&data)
+                        }
+
+                        #common
+
+                        /// Reference to the payload.
+                        pub fn bytes (&self) -> &[u8]{
+                            self.0
+                        }
+                    }
+                });
+            } else {
+                // Handle zero-length payloads
+                payload_struct_impls.push(quote! {
+                    #[derive(Debug, PartialEq, Eq)]
+                    #[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+                    #( #doc )*
+                    pub struct #t();
+
+                    impl #t {
+                        /// Create new
+                        pub fn new(_: &[u8]) -> #t {
+                            #t()
+                        }
+                        /// Create from raw_bytes (for compatibility with non-zero length payloads)
+                        pub fn new_from_raw(_: &[u8]) -> #t {
+                            #t()
+                        }
+
+                        #common
+
+                        /// Reference to the empty payload.
+                        pub fn bytes(&self) -> &[u8] {
+                            &[]
+                        }
+                    }
+                });
+            }
         }
     }
 
@@ -156,7 +194,7 @@ pub fn derive_command_handler(input: proc_macro::TokenStream) -> proc_macro::Tok
     .into()
 }
 
-// Parse enum variant arguments into `[Payload]` objects
+// Parse enum variant argument into `[Payload]` objects
 // For example:
 //
 // ```
@@ -179,7 +217,7 @@ fn parse_variant_fields(input: &syn::Type) -> Payload {
                     }
                     match &e.args[0] {
                         syn::GenericArgument::Lifetime(lt) => {
-                            Payload::new(var.clone(), Some(lt.ident.clone()))
+                            Payload::new(var.clone(), Some(lt.clone()))
                         }
                         _ => todo!("???"),
                     }
@@ -198,20 +236,23 @@ fn parse_variant_attrs(attrs: &Vec<syn::Attribute>) -> Attributes {
     let mut cid = None;
     let mut len = None;
     for attr in attrs {
+        // Only support `#[doc(...)]`
         if attr.path().is_ident("doc") {
             doc.push(attr.clone());
             continue;
         }
+        // ... and `#[cmd(...)]` arguments
         if !attr.path().is_ident("cmd") {
             continue;
         }
-        // Parse arguments
         if let Ok(nested) = attr
             .parse_args_with(syn::punctuated::Punctuated::<Meta, syn::Token![,]>::parse_terminated)
         {
-            // We'll only expect NameValues (ie. val=xx)
             for meta in nested {
                 match meta {
+                    Meta::Path(_) => unimplemented!("Meta::Path is not supported!"),
+                    Meta::List(_) => unimplemented!("Meta::List is not supported!"),
+                    // We'll only expect NameValues (ie. val=xx)
                     Meta::NameValue(v) => {
                         if let Some(id) = v.path.get_ident() {
                             match id.to_string().as_str() {
@@ -222,30 +263,36 @@ fn parse_variant_attrs(attrs: &Vec<syn::Attribute>) -> Attributes {
                                     len = Some(v.value);
                                 }
                                 &_ => {
-                                    eprintln!("Unhandled argument: {}", &id);
+                                    panic!("Invalid argument: {}", &id);
                                 }
                             }
                         } else {
                             panic!("Missing ident?");
                         }
                     }
-                    Meta::Path(_) => unimplemented!("Meta::Path is not supported!"),
-                    Meta::List(_) => unimplemented!("Meta::List is not supported!"),
                 }
             }
         }
     }
-    Attributes { doc, cid, len }
+    let attrs = if let (Some(cid), Some(len)) = (cid, len) {
+        Some((cid, len))
+    } else {
+        None
+    };
+    Attributes { doc, attrs }
 }
 
 // Parse enum variant into list of (Variant, Payload, Attributes) tuples.
 // For example:
 // ```
 // enum Foo<'a> {
+//   /// Field documentation.
 //   #[cmd(cid=0x1, len=1)]
-//   FieldA(A),     # (FieldA, Payload { name: A, lifetime: None }, Attributes)
+//   Var(A),        # (VarA, Payload { name: A, lifetime: None }, Attributes)
+//   /// Field documentation...
+//   /// ...continued.
 //   #[cmd(cid=0x2, len=5)]
-//   FieldB(B<'a>), # (FieldB, Payload { name: B, lifetime: Some(a) }, Attributes)
+//   VarB(B<'a>),   # (VarB, Payload { name: B, lifetime: Some(a) }, Attributes)
 // }
 // ```
 fn parse_enum_members(input: &DeriveInput) -> Vec<(Ident, Payload, Attributes)> {
@@ -253,7 +300,6 @@ fn parse_enum_members(input: &DeriveInput) -> Vec<(Ident, Payload, Attributes)> 
     match input.data {
         Data::Enum(ref item) => {
             for elem in item.variants.clone() {
-                // eprintln!("{:#?}", elem);
                 if elem.fields.len() != 1 {
                     panic!("Expecting single argument for {}", elem.ident)
                 }
@@ -266,6 +312,6 @@ fn parse_enum_members(input: &DeriveInput) -> Vec<(Ident, Payload, Attributes)> 
         }
         _ => panic!("Unsupported!"),
     };
-    // TODO: Sanity checks like missing cid/len and wrong values
+    // TODO: Sanity checks like missing cid/len and wrong values (u8)?
     items
 }
