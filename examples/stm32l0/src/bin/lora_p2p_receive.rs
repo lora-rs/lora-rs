de216bd2a1e04871b35c21bd06bfccd1554d7891
Priit Laes <plaes@plaes.org>
2025-01-08 12:12:13 +0200

    examples: stm32l0: Bump embassy stack and handle API changes

--- examples/stm32l0/src/bin/lora_p2p_receive.rs
+++ examples/stm32l0/src/bin/lora_p2p_receive.rs
@@ -5,14 +5,14 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::exti::{Channel, ExtiInput};
-use embassy_stm32::gpio::{Input, Level, Output, Pin, Pull, Speed};
+use embassy_stm32::exti::ExtiInput;
+use embassy_stm32::gpio::{Level, Output, Pin, Pull, Speed};
 use embassy_stm32::spi;
 use embassy_stm32::time::khz;
 use embassy_time::{Delay, Timer};
 use embedded_hal_bus::spi::ExclusiveDevice;
 use lora_phy::iv::GenericSx127xInterfaceVariant;
-use lora_phy::sx127x::{Sx127x, Sx1276};
+use lora_phy::sx127x::{Sx1276, Sx127x};
 use lora_phy::{mod_params::*, sx127x};
 use lora_phy::{LoRa, RxMode};
 use {defmt_rtt as _, panic_probe as _};
@@ -23,13 +23,12 @@ const LORA_FREQUENCY_IN_HZ: u32 = 903_900_000; // warning: set this appropriatel
 async fn main(_spawner: Spawner) {
     let mut config = embassy_stm32::Config::default();
     config.rcc.hsi = true;
-    config.rcc.mux = embassy_stm32::rcc::ClockSrc::HSI;
+    config.rcc.sys = embassy_stm32::rcc::Sysclk::HSI;
     let p = embassy_stm32::init(config);
 
     let nss = Output::new(p.PA15.degrade(), Level::High, Speed::Low);
     let reset = Output::new(p.PC0.degrade(), Level::High, Speed::Low);
-    let irq_pin = Input::new(p.PB4.degrade(), Pull::Up);
-    let irq = ExtiInput::new(irq_pin, p.EXTI4.degrade());
+    let irq = ExtiInput::new(p.PB4, p.EXTI4, Pull::Up);
 
     let mut spi_config = spi::Config::default();
     spi_config.frequency = khz(200);
