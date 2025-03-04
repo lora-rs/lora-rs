c12eb3d74a7c77a3769a5e99bff5ea35fcdf3b39
Priit Laes <plaes@plaes.org>
2025-03-04 09:27:16 +0200

    device: Add missing feature="class-c"

--- lorawan-device/src/mac/mod.rs
+++ lorawan-device/src/mac/mod.rs
@@ -265,6 +265,7 @@ impl Mac {
     /// Handles a received RF frame during RXC window. Returns None if unparseable, fails decryption,
     /// or fails MIC verification. Upon successful data rx, provides Response::DownlinkReceived.
     /// User must later call `take_downlink()` on the device to get the application data.
+    #[cfg(feature = "class-c")]
     pub(crate) fn handle_rxc<C: CryptoFactory + Default, const N: usize, const D: usize>(
         &mut self,
         buf: &mut RadioBuffer<N>,
