6396dd2116f95f3f11486b3d68045caed5cacccb
Priit Laes <plaes@plaes.org>
2025-06-07 21:18:16 +0300

    async_device: Add test to check oversized frame RX

--- lorawan-device/src/async_device/test/certification/mod.rs
+++ lorawan-device/src/async_device/test/certification/mod.rs
@@ -15,6 +15,7 @@ mod mac_common;
 
 mod dlchannelreq_eu868;
 mod newchannelreq_eu868;
+mod oversized_payload_eu868;
 mod rxparamsetup_eu868;
 
 /// Decrypts the payload allowing access to payload contents
