3a22f75aec6104c0119300ab565708e8928bc6d0
Peter Sallai <peter.sallai@badaccess.eu>
2025-10-12 21:16:11 +0200

    docs: Added warning to Txco setting, because it can hang tx,rx functions

--- examples/rp/src/bin/lora_p2p_receive.rs
+++ examples/rp/src/bin/lora_p2p_receive.rs
@@ -38,6 +38,10 @@ async fn main(_spawner: Spawner) {
     );
     let spi = ExclusiveDevice::new(spi, nss, Delay).unwrap();
 
+    // IMPORTANT: The TCXO configuration must match your board's hardware.
+    // If your board does not have a TCXO (Temperature-Compensated Crystal Oscillator),
+    // set `tcxo_ctrl` to `None`. An incorrect setting will cause receiving
+    // functions (e.g., `lora.rx()`) to hang indefinitely.
     let config = sx126x::Config {
         chip: Sx1262,
         tcxo_ctrl: Some(TcxoCtrlVoltage::Ctrl1V7),
